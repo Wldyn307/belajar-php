a200c8148392222dd86d8c0a37d82134640cf4e5
WaliyudinHelmi <110141304+Wldyn307@users.noreply.github.com>
2022-08-24 07:13:05 +0700

    Add files via upload

---
+++ helmi.php
@@ -0,0 +1,59 @@
+<?php
+    echo "
+      <marquee>
+           <h1>WALIYUDIN HELMI<qf/h1>
+      </marquee>";
+
+
+
+
+    // variable
+    $nama = "waliyudin helmi";
+    $kelas = "XI RPL 2";
+    $nilai = 99.99;
+
+    //
+    echo "nama : ".$nama;
+    echo "<br>";
+    echo "kelas : ".$kelas;
+    echo "<br>";
+    echo "nilai : $nilai";
+    echo "<br>";
+
+    //operator jumlah
+    echo "<br><br>";
+    $angka1 = "5";
+    $angka2 = "10";
+
+    $hasil = $angka1 + $angka2;
+    echo "hasil dari penjumlahan : $hasil";
+
+    //Rumus Luas segitiga
+    echo "<br><br>";
+    $alas = 8;
+    $tinggi = 20;
+
+    $rumus = 0.5 * $alas * $tinggi;
+    echo "hasil rumus : $rumus";
+
+    //RUMUS LINGKARAN
+    echo "<br><br>";
+    $phi = 3.14;
+    $r = 7;
+    //luas lingkaran : phi x r x r
+    $lingkaran = $phi * $r * $r;
+
+    echo "hasil luas lingkaran : $lingkaran";
+
+    //rumus volume balok
+    echo "<br><br>";
+    $p= 18;
+    $l= 12;
+    $t= 9;
+
+    $volume_balok = $p * $l * $t;
+    echo "hasil volume balok : $volume_balok";
+
+?>
+
+
